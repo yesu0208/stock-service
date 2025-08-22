ea2bb101400dc65da691a112e512ea4a672a7fbe
yesu0208 <dltmddnr0208@gmail.com>
2025-08-22 17:27:07 +0900

    #23 - GithubUser 생성
    인증 정보를 받아오게 되면 OAuth2User 인터페이스의 구현체로 받아옴. (네이버든, 카카오든, ..)
    지금은 github 인증만 구현하고 있으니, 뽑아야 될 정보가 명확히 정해져 있다. (github에 맞춰서)
    -> 그 부분을 미리 정의함. (가져올 정보를 미리 정의) : login, name, email

---
+++ src/main/java/arile/toy/stock_service/dto/security/GithubUser.java
@@ -0,0 +1,33 @@
+package arile.toy.stock_service.dto.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public record GithubUser(
+        String id,
+        String name,
+        String email
+) implements OAuth2User {
+
+    public static GithubUser from(Map<String, Object> attributes) {
+        return new GithubUser( // 필요한 값만 추출
+                String.valueOf(attributes.get("login")),
+                String.valueOf(attributes.get("name")), // nullable
+                String.valueOf(attributes.get("email")) // nullable
+        );
+    }
+
+    // 뽑을 정보는 위에 있으니, 따로 구현 x (나머지 정보는 버림)
+    @Override public Map<String, Object> getAttributes() {return Map.of();}
+    // 딱히 필요 x (권한 : admin, user, ...)
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {return List.of();}
+
+    @Override // 닉네임이 세팅 안되면 name에 "null" 문자열이 내려옴.
+    public String getName() {
+        return name.equals("null") ? id : name; // name이 없다면 id, 있으면 name을 그대로 반환
+    }
+}
