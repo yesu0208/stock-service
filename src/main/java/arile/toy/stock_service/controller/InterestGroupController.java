b4d9dbcf87cee319ecff20b2aeb4c65728a76fdd
yesu0208 <dltmddnr0208@gmail.com>
2025-08-22 14:56:13 +0900

    #17 - 내 관심그룹 목록(my-groups) 뷰 생성
    chat gpt의 도움을 받아, 내 관심그룹 목록 뷰를 html 파일로 생성함.
    아직 다크모드 여부는 적용 안함.
    내 관심그룹 이름을 누르면, 해당 관심그룹으로 이동함.
    (/interest-group?groupName=****)
    이를 위해, 단일 interest group을 조회하는 GET API에 groupName을 @RequestParam을 주었다.
    (required=false로 값을 안 주었을 경우 default를 보여주도록 한다.)
    이에 맞춰, defaultInterestGroup method도 수정하였다.
    (String parameter를 준 경우에는 해당 groupName을 반영한 형태로, 주지 않은 경우 기본 값을 반영한 형태로)

--- src/main/java/arile/toy/stock_service/controller/InterestGroupController.java
+++ src/main/java/arile/toy/stock_service/controller/InterestGroupController.java
@@ -4,15 +4,18 @@ import arile.toy.stock_service.domain.StockInfo;
 import arile.toy.stock_service.dto.request.InterestGroupRequest;
 import arile.toy.stock_service.dto.response.InterestGroupResponse;
 import arile.toy.stock_service.dto.response.InterestStockResponse;
+import arile.toy.stock_service.dto.response.SimpleInterestGroupResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import arile.toy.stock_service.repository.StockInfoRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,8 +26,10 @@ public class InterestGroupController {
 
     // 단일 interest group 조회
     @GetMapping("/interest-group")
-    public String interestGroup(Model model) { // redirect를 위해 parameter 후가
-        var interestGroup = defaultInterestGroup();
+    public String interestGroup(
+            @RequestParam(required = false) String groupName, // 필수값이 아닌 옵션값 (내 관심그룹 목록 페이지에서 관심그룹 이름 클릭 시에만 적용)
+            Model model) { // redirect를 위해 parameter 후가
+        var interestGroup = defaultInterestGroup(groupName);
 
         List<StockInfo> stockInfo = stockInfoRepository.findAll();
         List<String> stockNames = stockInfo.stream()
@@ -50,7 +55,11 @@ public class InterestGroupController {
 
     // 내 interest group 목록 조회
     @GetMapping("/interest-group/my-groups")
-    public String myGroups() {
+    public String myGroups(Model model) {
+        var interestGroups = mySampleGroups();
+
+        model.addAttribute("interestGroups", interestGroups);
+
         return "my-groups";
     }
 
@@ -68,13 +77,22 @@ public class InterestGroupController {
 
 
     // 기본 interest group
-    private InterestGroupResponse defaultInterestGroup() {
+    private InterestGroupResponse defaultInterestGroup(String groupName) {
         return new InterestGroupResponse(
-                "group_name",
+                groupName != null ? groupName : "group_name", // groupName 받았으면 그대로 쓰고, 안받았으면 기본값
                 "Arile",
                 List.of(
                         new InterestStockResponse("삼성전자보통주", null, null, null, 1)
                 )
+        );
+    }
+
+    // sample group 목록
+    private static List<SimpleInterestGroupResponse> mySampleGroups() {
+        return List.of(
+                new SimpleInterestGroupResponse("group_name1", "Arile", LocalDate.of(2025, 1, 1).atStartOfDay()),
+                new SimpleInterestGroupResponse("group_name2", "Arile", LocalDate.of(2025, 2, 2).atStartOfDay()),
+                new SimpleInterestGroupResponse("group_name3", "Arile", LocalDate.of(2025, 3, 3).atStartOfDay())
         );
     }
 
