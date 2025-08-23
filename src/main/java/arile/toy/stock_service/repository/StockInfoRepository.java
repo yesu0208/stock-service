4b07def20acc14550c7f9298c0a379df39b9e979
yesu0208 <dltmddnr0208@gmail.com>
2025-08-23 21:11:28 +0900

    # 31 : 외부 API 사용을 위해 종목명으로 종목코드 가져오는 StockInfoService
    외부 API를 사용하기 위해 종목코드(단축코드 : shortCode)를 알아야 하는데, 이러한 정보는 DB에 저장되어 있으므로
    서비스 레이어에서 이 정보를 가져오도록 함.
    StockInfoRepository에도 findByStockName  추가함.

--- src/main/java/arile/toy/stock_service/repository/StockInfoRepository.java
+++ src/main/java/arile/toy/stock_service/repository/StockInfoRepository.java
@@ -1,7 +1,11 @@
 package arile.toy.stock_service.repository;
 
+import arile.toy.stock_service.domain.InterestGroup;
 import arile.toy.stock_service.domain.StockInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockInfoRepository extends JpaRepository<StockInfo, Long> {
+    Optional<StockInfo> findByStockName(String stockName);
 }
