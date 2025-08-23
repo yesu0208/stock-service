9e25f24fa7625981e49f339b779faf2a1219f530
yesu0208 <dltmddnr0208@gmail.com>
2025-08-23 21:17:12 +0900

    #31 - 새로 만들 CurrentStockInfoService에서 필요한 record 생성
    request로 받을 데이터는 그대로 유지한 채로
    user에게 response로 보낼 때 기존의 Dto, response에
    외부 API에서 받은 데이터(현재가, 상승폭, ..)들을 추가한
    Dto와 response를 만듦.
    기존 이름에 WithCurrentInfo를 추가하여 기존의 dto와 response와 구분지었음.

---
+++ src/main/java/arile/toy/stock_service/dto/InterestStockWithCurrentInfoDto.java
@@ -0,0 +1,60 @@
+package arile.toy.stock_service.dto;
+
+import java.time.LocalDateTime;
+
+public record InterestStockWithCurrentInfoDto(
+        Long id,
+        String stockName,
+        Integer buyingPrice,
+        Integer numOfStocks,
+        Integer breakEvenPrice,
+        Integer totalBuyingPrice,
+        Integer fieldOrder,
+
+        Integer nowValue,
+        Integer changeValue,
+        String rise2OrFall1,
+
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+    // static method (전체)
+    public static InterestStockWithCurrentInfoDto of(
+            Long id,
+            String stockName,
+            Integer buyingPrice,
+            Integer numOfStocks,
+            Integer breakEvenPrice,
+            Integer totalBuyingPrice,
+            Integer fieldOrder,
+
+            Integer nowValue,
+            Integer changeValue,
+            String rise2OrFall1,
+
+            LocalDateTime createdAt,
+            String createdBy,
+            LocalDateTime modifiedAt,
+            String modifiedBy
+    ) {
+        return new InterestStockWithCurrentInfoDto(id, stockName, buyingPrice, numOfStocks,breakEvenPrice,totalBuyingPrice, fieldOrder, nowValue, changeValue, rise2OrFall1, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    // static method (일부)
+    public static InterestStockWithCurrentInfoDto of(
+            String stockName,
+            Integer buyingPrice,
+            Integer numOfStocks,
+            Integer breakEvenPrice,
+            Integer totalBuyingPrice,
+            Integer fieldOrder,
+
+            Integer nowValue,
+            Integer changeValue,
+            String rise2OrFall1
+    ) {
+        return new InterestStockWithCurrentInfoDto(null, stockName, buyingPrice, numOfStocks, breakEvenPrice, totalBuyingPrice, fieldOrder, nowValue, changeValue, rise2OrFall1, null, null, null, null);
+    }
+}
---
+++ src/main/java/arile/toy/stock_service/dto/response/InterestStockWithCurrentInfoResponse.java
@@ -0,0 +1,30 @@
+package arile.toy.stock_service.dto.response;
+
+import arile.toy.stock_service.dto.InterestStockWithCurrentInfoDto;
+
+public record InterestStockWithCurrentInfoResponse(
+        String stockName,
+        Integer buyingPrice,
+        Integer numOfStocks,
+        Integer breakEvenPrice,
+        Integer totalBuyingPrice,
+        Integer fieldOrder,
+        Integer nowValue,
+        Integer changeValue,
+        String rise2OrFall1
+) {
+    // Dto -> response
+    public static InterestStockWithCurrentInfoResponse fromDto(InterestStockWithCurrentInfoDto dto) {
+        return new InterestStockWithCurrentInfoResponse(
+                dto.stockName(),
+                dto.buyingPrice(),
+                dto.numOfStocks(),
+                dto.breakEvenPrice(),
+                dto.totalBuyingPrice(),
+                dto.fieldOrder(),
+                dto.nowValue(),
+                dto.changeValue(),
+                dto.rise2OrFall1()
+        );
+    }
+}
