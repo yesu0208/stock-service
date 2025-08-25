aa04416019c9351afc7be11be20e9fe5ce58e454
yesu0208 <dltmddnr0208@gmail.com>
2025-08-25 22:17:40 +0900

    # 39 - 현재 종목 정보를 받기 위해 외부 API 호출 : service layer
    CurrentStockInfoService는 stockName(종목명)을 받아 해당 종목명에 해당하는 종목 정보를 받는 getCurrentStockInfo method를 갖고 있다.
    
    API를 호출하기 위해서는 종목 코드를 알고 있어야 하나
    유저 입장에서는 종목명으로 해당 정보에 접근해야 하므로
    해당 service layer에서 StockInfoService에 접근해 종목 코드를 가져온 후, 이를 API 호출에 사용하였음.
    
    API 호출 이후 가져온 JSON data를 적절히 객체로 변환 후, data를 CurrentStockInfoDto에 저장함.

---
+++ src/main/java/arile/toy/stock_service/service/CurrentStockInfoService.java
@@ -0,0 +1,102 @@
+package arile.toy.stock_service.service;
+
+import arile.toy.stock_service.domain.naverstock.NaverStockResponse;
+import arile.toy.stock_service.dto.CurrentStockInfoDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Service
+public class CurrentStockInfoService {
+
+    private final RestClient restClient = RestClient.create();
+    private final StockInfoService stockInfoService;
+
+    public CurrentStockInfoDto getCurrentStockInfo(String stockName) {
+
+        String shortCode = stockInfoService.loadShortCodeByStockName(stockName);
+
+        String stringResponse = restClient
+                .get()
+                .uri("https://polling.finance.naver.com/api/realtime?query=SERVICE_ITEM:" + shortCode)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    // 외부 API 호출 예외 처리(실패할 경우)
+                    throw new RuntimeException(); // 추후 예외 추가
+                })
+                .body(String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        NaverStockResponse response;
+        try {
+            response = mapper.readValue(stringResponse, NaverStockResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e); // 추후 예외 추가
+        }
+
+
+        // response로부터 data 추출
+        var stockData = response.result().areas().getFirst().datas().getFirst();
+
+        String marketState = stockData.ms();
+        String marketType = stockData.mt();
+        Integer nowValue = stockData.nv();
+        Integer changeValue = stockData.cv();
+        Double changeRate = stockData.cr();
+        String riseOrFall = stockData.rf();
+        Integer highestValue = stockData.hv();
+        Integer lowestValue = stockData.lv();
+        Integer upperLimit = stockData.ul();
+        Integer lowerLimit = stockData.ll();
+        Integer openValue = stockData.ov();
+        Integer standardValue = stockData.sv();
+        Long transactionVolume = stockData.aq();
+        Long transactionValue = stockData.aa();
+        Long unixTimeMs = response.result().time();
+        LocalDateTime time = unixTimeMsToLocalDateTime(unixTimeMs);
+
+
+
+        // rf(rise or fall)에 따라서 cv(change value)의 부호 변환
+        changeValue = (Objects.equals(riseOrFall, "1") || Objects.equals(riseOrFall, "2")) ? changeValue : -changeValue;
+        changeRate = (Objects.equals(riseOrFall, "1") || Objects.equals(riseOrFall, "2")) ? changeRate : -changeRate;
+
+
+        return CurrentStockInfoDto.of(
+                shortCode,
+                stockName,
+                marketState,
+                marketType,
+                nowValue,
+                changeValue,
+                changeRate,
+                riseOrFall,
+                highestValue,
+                lowestValue,
+                upperLimit,
+                lowerLimit,
+                openValue,
+                standardValue,
+                transactionVolume,
+                transactionValue,
+                time
+        );
+    }
+
+    public LocalDateTime unixTimeMsToLocalDateTime(Long unixTimeMs) {
+
+        return Instant.ofEpochMilli(unixTimeMs)
+                .atZone(ZoneId.systemDefault()) // 현재 JVM 기본 타임존 (한국이면 Asia/Seoul)
+                .toLocalDateTime();
+    }
+
+}
