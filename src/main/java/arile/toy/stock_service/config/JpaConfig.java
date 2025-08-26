d15700495d252ca24bbd45d7e4c46d1642d7602d
yesu0208 <dltmddnr0208@gmail.com>
2025-08-26 23:33:17 +0900

    # 41 - JpaConfig 수정
    기존에는 Auditing Fields의 createdBy, modifiedBy가 하드코딩된 형태였으나,
     이 부분을 현재 인증정보를 바탕으로 실제 name을 반영하도록 함.

--- src/main/java/arile/toy/stock_service/config/JpaConfig.java
+++ src/main/java/arile/toy/stock_service/config/JpaConfig.java
@@ -1,9 +1,12 @@
 package arile.toy.stock_service.config;
 
+import arile.toy.stock_service.dto.security.GithubUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,6 +16,20 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("arile");
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof GithubUser githubUser) {
+                return Optional.of(githubUser.getName()); // github login ID
+            }
+
+            return Optional.of(authentication.getName()); // fallback
+        };
     }
 }
