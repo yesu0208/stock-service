c3fdb34b24def496e765cb38f6df707f3bd78326
yesu0208 <dltmddnr0208@gmail.com>
2025-08-26 23:02:50 +0900

    # 41 - GithubUserInfo 생성
    기존에는 OAuth2를 이용하여 로그인만 진행하였고 따로 유저 정보를 db에 저장하지 않았음.
    하지만, 개별 유저와 관련된 정보(ex : 수수료, 전체 매입 금액, 마지막 로그인 일자 등)을 관리하기 위해 따로 유저 정보들을 db에 저장할 필요성을 느낌.
    또한, 추후 서비스 확장에 대비하기 위해, 유저 정보 entity인 GithubUserInfo 생성.
    + repository 생성

---
+++ src/main/java/arile/toy/stock_service/domain/GithubUserInfo.java
@@ -0,0 +1,38 @@
+package arile.toy.stock_service.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+@Table(name = "github_user_information")
+@Entity
+public class GithubUserInfo {
+
+    @Id
+    private Long unchangeableId;
+
+    @Column(nullable = false)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private LocalDateTime lastLoginAt;
+
+    @Column
+    private Double fee;
+
+}
