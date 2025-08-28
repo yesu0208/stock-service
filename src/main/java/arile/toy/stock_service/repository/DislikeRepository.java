28b35c42c90577c2a63eb353c1d926a8e186b13e
yesu0208 <dltmddnr0208@gmail.com>
2025-08-28 21:26:12 +0900

    # 46 - 게시물 싫어요 기능 추가 : DislikeRepository 생성

---
+++ src/main/java/arile/toy/stock_service/repository/DislikeRepository.java
@@ -0,0 +1,13 @@
+package arile.toy.stock_service.repository;
+
+import arile.toy.stock_service.domain.Dislike;
+import arile.toy.stock_service.domain.GithubUserInfo;
+import arile.toy.stock_service.domain.Like;
+import arile.toy.stock_service.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DislikeRepository extends JpaRepository<Dislike, Long> {
+    Optional<Dislike> findByGithubUserInfoAndPost(GithubUserInfo githubUserInfo, Post post);
+}
