2b637437d4704e9a239b4d457404d865072f5fc0
yesu0208 <dltmddnr0208@gmail.com>
2025-08-28 21:29:42 +0900

    # 46 - 게시물 좋아요/싫어요 기능 추가 : service layer 구현
    service layer에서 구현 시 주의사항은,
    만약 싫어요를 누른 유저가 좋아요 버튼을 누를 때
    싫어요는 취소하면서 좋아요가 눌러야 함.
    (좋아요, 싫어요는 동시에 누를 수 없음.)

--- src/main/java/arile/toy/stock_service/service/PostService.java
+++ src/main/java/arile/toy/stock_service/service/PostService.java
@@ -1,12 +1,17 @@
 package arile.toy.stock_service.service;
 
+import arile.toy.stock_service.domain.Dislike;
 import arile.toy.stock_service.domain.GithubUserInfo;
+import arile.toy.stock_service.domain.Like;
 import arile.toy.stock_service.domain.post.Post;
 import arile.toy.stock_service.dto.InterestGroupDto;
 import arile.toy.stock_service.dto.PostDto;
 import arile.toy.stock_service.dto.SimplePostDto;
+import arile.toy.stock_service.dto.response.PostResponse;
 import arile.toy.stock_service.dto.security.GithubUser;
+import arile.toy.stock_service.repository.DislikeRepository;
 import arile.toy.stock_service.repository.GithubUserInfoRepository;
+import arile.toy.stock_service.repository.LikeRepository;
 import arile.toy.stock_service.repository.PostRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +27,24 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final GithubUserInfoRepository githubUserInfoRepository;
+    private final LikeRepository likeRepository;
+    private final DislikeRepository dislikeRepository;
 
     @Transactional(readOnly = true)
-    public PostDto loadPost(Long postId) {
-        return postRepository.findById(postId)
-                .map(PostDto::fromEntity)
+    public PostResponse loadPost(String unchangeableId, Long postId) {
+        var post = postRepository.findById(postId)
                 // Optional
                 .orElseThrow(() -> new EntityNotFoundException("해당 게시물이 없습니다 - postId: " + postId));
+
+        var githubUserInfo = githubUserInfoRepository.findById(unchangeableId)
+                // Optional
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 없습니다 - postId: " + unchangeableId));
+
+        Boolean isLiking = likeRepository.findByGithubUserInfoAndPost(githubUserInfo, post).isPresent();
+
+        Boolean isDisliking = dislikeRepository.findByGithubUserInfoAndPost(githubUserInfo, post).isPresent();
+
+        return PostResponse.fromDto(PostDto.fromEntity(post), isLiking, isDisliking);
     }
 
 
@@ -67,6 +83,60 @@ public class PostService {
 
     public void deletePost(String unchangeableId, Long postId) {
         postRepository.deleteByUserUnchangeableIdAndPostId(unchangeableId, postId);
+    }
+
+
+    public PostResponse toggleLike(Long postId, String unchangeableId) {
+        var post = postRepository.findById(postId) // Optional<PostEntity>
+                .orElseThrow(() -> new EntityNotFoundException("해당 게시물이 없습니다 "));
+
+        var githubUserInfo = githubUserInfoRepository.findById(unchangeableId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 없습니다 "));
+
+        var like = likeRepository.findByGithubUserInfoAndPost(githubUserInfo, post); // 좋아요를 눌렀거나, 안눌렀거나(Optional)
+
+        var dislike = dislikeRepository.findByGithubUserInfoAndPost(githubUserInfo, post);
+
+        if (like.isPresent()) { // 이미 존재하면, 삭제
+            likeRepository.delete(like.get());
+            post.setLikesCount(Math.max(0, post.getLikesCount() - 1));
+            return PostResponse.fromDto(PostDto.fromEntity(postRepository.save(post)), false, false);
+        } else { // 존재하지 않는다면, 추가
+            likeRepository.save(Like.of(githubUserInfo, post));
+            if (dislike.isPresent()) { // 만약, 싫어요를 눌렀다면 싫어요를 취소하고 좋아요로 변환
+                dislikeRepository.delete(dislike.get());
+                post.setDislikesCount(post.getDislikesCount() - 1);
+            }
+            post.setLikesCount(post.getLikesCount() + 1);
+            return PostResponse.fromDto(PostDto.fromEntity(postRepository.save(post)), true, false);
+        }
+    }
+
+
+    public PostResponse toggleDislike(Long postId, String unchangeableId) {
+        var post = postRepository.findById(postId) // Optional<PostEntity>
+                .orElseThrow(() -> new EntityNotFoundException("해당 게시물이 없습니다 "));
+
+        var githubUserInfo = githubUserInfoRepository.findById(unchangeableId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 없습니다 "));
+
+        var like = likeRepository.findByGithubUserInfoAndPost(githubUserInfo, post); // 좋아요를 눌렀거나, 안눌렀거나(Optional)
+
+        var dislike = dislikeRepository.findByGithubUserInfoAndPost(githubUserInfo, post);
+
+        if (dislike.isPresent()) { // 이미 존재하면, 삭제
+            dislikeRepository.delete(dislike.get());
+            post.setDislikesCount(Math.max(0, post.getDislikesCount() - 1));
+            return PostResponse.fromDto(PostDto.fromEntity(postRepository.save(post)), false, false);
+        } else { // 존재하지 않는다면, 추가
+            dislikeRepository.save(Dislike.of(githubUserInfo, post));
+            if (like.isPresent()) { // 만약, 싫어요를 눌렀다면 싫어요를 취소하고 좋아요로 변환
+                likeRepository.delete(like.get());
+                post.setLikesCount(post.getLikesCount() - 1);
+            }
+            post.setDislikesCount(post.getDislikesCount() + 1);
+            return PostResponse.fromDto(PostDto.fromEntity(postRepository.save(post)), false, true);
+        }
     }
 
 
