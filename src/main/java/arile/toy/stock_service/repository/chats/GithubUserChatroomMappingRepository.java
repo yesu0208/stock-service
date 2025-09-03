f9bffaf2e7df58b9bef67c9322728a79bf978430
yesu0208 <dltmddnr0208@gmail.com>
2025-09-03 19:50:27 +0900

    # 71 - service layer에서 필요한 query method 선언(repository)
    entity의 필드에 Chatroom이 있는데, 이 Chatroom 내부에 필드 chatroomId가 있음.
    이를 명시적으로 표현하기 위해 ChatroomChatroomId로 표현함.
    GithubUserInfoUnchangeableId도 같은 방식으로 표현함.

--- src/main/java/arile/toy/stock_service/repository/chats/GithubUserChatroomMappingRepository.java
+++ src/main/java/arile/toy/stock_service/repository/chats/GithubUserChatroomMappingRepository.java
@@ -3,5 +3,10 @@ package arile.toy.stock_service.repository.chats;
 import arile.toy.stock_service.domain.GithubUserChatroomMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GithubUserChatroomMappingRepository extends JpaRepository<GithubUserChatroomMapping, Long> {
+    Boolean existsByGithubUserInfoUnchangeableIdAndChatroomChatroomId(String unchangeableId, Long chatroomId);
+    void deleteByGithubUserInfoUnchangeableIdAndChatroomChatroomId(String unchangeableId, Long chatroomId);
+    List<GithubUserChatroomMapping> findAllByGithubUserInfoUnchangeableId(String unchangeableId);
 }
