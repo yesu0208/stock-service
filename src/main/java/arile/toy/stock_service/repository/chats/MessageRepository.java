6c0601813f24b8ccfaf110ab28b7bc85e1874959
yesu0208 <dltmddnr0208@gmail.com>
2025-09-03 21:01:40 +0900

    # 71 - service layer에 필요한 query method 선언

--- src/main/java/arile/toy/stock_service/repository/chats/MessageRepository.java
+++ src/main/java/arile/toy/stock_service/repository/chats/MessageRepository.java
@@ -3,5 +3,8 @@ package arile.toy.stock_service.repository.chats;
 import arile.toy.stock_service.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findAllByChatroomId(Long chatroomId);
 }
