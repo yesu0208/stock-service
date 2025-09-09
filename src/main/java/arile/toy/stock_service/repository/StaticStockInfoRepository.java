bc866603851c52388c9927f7d6fed62cb70edca8
yesu0208 <dltmddnr0208@gmail.com>
2025-09-09 17:54:25 +0900

    # 83 - Caching 기법 적용
    같은 종목 관련 정보를 매번 query를 날려 DB에서 가져오는 것은 비효율적임.
    종목 정보를 가져오는 페이지마다 이 query가 발생하여 다른 페이지에 비해 훨씬 느리다는 것을 확인함. (약 1.3s)
    따라서, business logic에서 변경할 여지가 없는 정적인 종목 정보는 메모리의 cache를 활용하여 데이터를 가져오도록 함.
    현재 heroku에서는 단일 dyno만 사용 중이므로, redis와 같은 외부 cache까지는 필요 없다고 판단함. 이후 필요성이 생기면 redis 사용할 예정,

--- src/main/java/arile/toy/stock_service/repository/StaticStockInfoRepository.java
+++ src/main/java/arile/toy/stock_service/repository/StaticStockInfoRepository.java
@@ -1,10 +1,12 @@
 package arile.toy.stock_service.repository;
 
 import arile.toy.stock_service.domain.StaticStockInfo;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface StaticStockInfoRepository extends JpaRepository<StaticStockInfo, Long> {
+    @Cacheable("stockNames")
     Optional<StaticStockInfo> findByStockName(String stockName);
 }
