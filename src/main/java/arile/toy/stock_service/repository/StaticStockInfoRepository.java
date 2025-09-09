a6b9717e28b454221a9db9c35bf9c85a3740d732
yesu0208 <dltmddnr0208@gmail.com>
2025-09-09 20:20:29 +0900

    # 89 - 잘못된 코드 수정
    앞서 @Cacheable annotation을 의도한 것과 다른 query method에 적용한 실수가 있었음. 이를 수정함.

--- src/main/java/arile/toy/stock_service/repository/StaticStockInfoRepository.java
+++ src/main/java/arile/toy/stock_service/repository/StaticStockInfoRepository.java
@@ -3,10 +3,15 @@ package arile.toy.stock_service.repository;
 import arile.toy.stock_service.domain.StaticStockInfo;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StaticStockInfoRepository extends JpaRepository<StaticStockInfo, Long> {
-    @Cacheable(value = "stockNames", key = "'stockNames'")
+    @Override
+    @NonNull
+    @Cacheable(value = "stockNames")
+    List<StaticStockInfo> findAll();
     Optional<StaticStockInfo> findByStockName(String stockName);
 }
